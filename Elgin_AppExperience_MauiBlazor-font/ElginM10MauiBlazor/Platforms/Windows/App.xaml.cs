01a9a282816ccbbfe53b8e91c30ff151d0231e90
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-30 22:07:16 -0300

    Força um tamnho fixo na janela windows em 800x550, para ficar equivalente a aparência do M10 e não se preocupar com layout nesse momento

--- Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Platforms/Windows/App.xaml.cs
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Platforms/Windows/App.xaml.cs
@@ -9,6 +9,9 @@ namespace ElginM10MauiBlazor.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
+    const int WindowWidth = 800;
+    const int WindowHeight = 550;
+
     /// <summary>
     /// Initializes the singleton application object.  This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -16,6 +19,18 @@ public partial class App : MauiWinUIApplication
     public App()
     {
         this.InitializeComponent();
+
+        // Redimensiona a janela do windows //
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32(WindowWidth, WindowHeight));
+        });
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
