d9c02383559407578fb3889d3f4423a214308aee
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-31 14:54:08 -0300

    Criando página para impressão de imagens

--- Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Pages/ImpressoraImagem.razor.cs
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Pages/ImpressoraImagem.razor.cs
@@ -3,4 +3,24 @@
 namespace ElginM10MauiBlazor.Pages;
 public partial class ImpressoraImagem : ComponentBase
 {
+    [CascadingParameter]
+    private Impressora Parent { get; set; }
+
+    private readonly Dados _dados = new();
+
+    private async Task Imprimir()
+    {
+
+    }
+
+    private async Task CarregarImagem()
+    {
+
+    }
+
+    private class Dados
+    {
+        public bool CutPaper { get; set; } = false;
+
+    }
 }
