5a46bfc0e48b7de23c2a707889c5f156e242f387
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-30 22:15:34 -0300

    Criado o DialogService

---
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Services/DialogService.cs
@@ -0,0 +1,18 @@
+﻿namespace ElginM10MauiBlazor.Services;
+internal class DialogService : IDialogService
+{
+    public async Task DisplayAlert(string title, string message)
+        => await Application.Current.MainPage.DisplayAlert(title, message, "OK");
+
+    public async Task DisplayAlert(string title, string message, string cancel)
+        => await Application.Current.MainPage.DisplayAlert(title, message, cancel);
+
+    public async Task<bool> DisplayAlert(string title, string message, string accept, string cancel)
+        => await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+
+    public async Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = "")
+    => await Application.Current.MainPage.DisplayPromptAsync(title, message, accept, cancel, placeholder, maxLength, keyboard, initialValue);
+
+    public async Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons)
+        => await Application.Current.MainPage.DisplayActionSheet(title, cancel, destruction, buttons);
+}
---
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Services/IDialogService.cs
@@ -0,0 +1,10 @@
+﻿namespace ElginM10MauiBlazor.Services;
+
+internal interface IDialogService
+{
+    Task<string> DisplayActionSheet(string title, string cancel, string destruction, params string[] buttons);
+    Task DisplayAlert(string title, string message);
+    Task DisplayAlert(string title, string message, string cancel);
+    Task<bool> DisplayAlert(string title, string message, string accept, string cancel);
+    Task<string> DisplayPromptAsync(string title, string message, string accept = "OK", string cancel = "Cancel", string placeholder = null, int maxLength = -1, Keyboard keyboard = null, string initialValue = "");
+}
