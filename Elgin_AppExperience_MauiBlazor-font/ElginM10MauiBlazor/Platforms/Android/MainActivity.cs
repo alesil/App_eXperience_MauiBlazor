1fbfddfaaada37c24488d221e80ca258c0aee81e
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-30 22:05:06 -0300

    Contexto da activity principal

--- Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Platforms/Android/MainActivity.cs
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Platforms/Android/MainActivity.cs
@@ -6,4 +6,13 @@ namespace ElginM10MauiBlazor;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    // Contexto da Activity principal, torna-o acessível para os métodos que necessitam desse contexto.
+    public static MainActivity mContext;
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        mContext = this;
+    }
 }
