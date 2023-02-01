d46fa2828104a872a35c88e8de2a4dd0ef55a241
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-01-31 22:29:28 -0300

    Botão HOME, e mudança do botão KIOSK para o MainLayout (aparecendo em todas as telas)

--- Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Pages/Principal.razor.cs
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Pages/Principal.razor.cs
@@ -3,28 +3,5 @@
 namespace ElginM10MauiBlazor.Pages;
 public partial class Principal : ComponentBase
 {
-    private static bool KioskMode { get; set; } = false;
 
-    private void ToggleKioskMode()
-    {
-        if (DeviceInfo.Current.Platform != DevicePlatform.Android)
-        {
-            return;
-        }
-
-        KioskMode = !KioskMode;
-
-        if (KioskMode)
-        {
-            KioskService.DesabilitaBarraNavegacao();
-            KioskService.DesabilitaBarraStatus();
-            KioskService.DesabilitaBotaoPower();
-        }
-        else
-        {
-            KioskService.HabilitaBarraNavegacao();
-            KioskService.HabilitaBarraStatus();
-            KioskService.HabilitaBotaoPower();
-        }
-    }
 }
---
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Shared/MainLayout.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace ElginM10MauiBlazor.Shared;
+public partial class MainLayout
+{
+        private static bool KioskMode { get; set; } = false;
+
+    private void ToggleKioskMode()
+    {
+        if (DeviceInfo.Current.Platform != DevicePlatform.Android)
+        {
+            return;
+        }
+
+        KioskMode = !KioskMode;
+
+        if (KioskMode)
+        {
+            KioskService.DesabilitaBarraStatus();
+            KioskService.DesabilitaBarraNavegacao();
+            KioskService.DesabilitaBotaoPower();
+        }
+        else
+        {
+            KioskService.HabilitaBarraNavegacao();
+            KioskService.HabilitaBarraStatus();
+            KioskService.HabilitaBotaoPower();
+        }
+    }
+}
