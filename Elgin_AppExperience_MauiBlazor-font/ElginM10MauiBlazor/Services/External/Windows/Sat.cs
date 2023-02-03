9537666e30c6d2ef198aab59fd5dc3497e52523f
Alexandre Rodrigues da Silva <alexandre@alesil.com.br>
2023-02-03 14:44:43 -0300

    Comunicação SAT na plataforma Windows

---
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Services/External/Windows/Sat.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ElginM10MauiBlazor.Services.External.Windows;
+internal class Sat
+{
+    public const string DLL = @"Platforms\Windows\Library\dllsat.dll";
+
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr ConsultarStatusOperacional(int sessao, string cod);
+
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr EnviarDadosVenda(int sessao, string cod, string dados);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr CancelarUltimaVenda(int sessao, string cod, string chave, string dadoscancel);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr TesteFimAFim(int sessao, string cod, string dados);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr ConsultarSAT(int sessao);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr ConsultarNumeroSessao(int sessao, string cod, int sessao_a_ser_consultada);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr AtivarSAT(int sessao, int tipoCert, string cod_Ativacao, string cnpj, int uf);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr ComunicarCertificadoICPBRASIL(int sessao, string cod, string csr);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr ConfigurarInterfaceDeRede(int sessao, string cod, string xmlConfig);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr AssociarAssinatura(int sessao, string cod, string cnpj, string sign_cnpj);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr DesbloquearSAT(int sessao, string cod_ativacao);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr BloquearSAT(int sessao, string cod_ativacao);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr TrocarCodigoDeAtivacao(int sessao, string cod_ativacao, int opcao, string nova_senha, string conf_senha);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr ExtrairLogs(int sessao, string cod_ativacao);
+
+    [DllImport(DLL, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr AtualizarSoftwareSAT(int sessao, string cod_ativacao);
+}
--- Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Services/SatService.Windows.cs
+++ Elgin_AppExperience_MauiBlazor-font/ElginM10MauiBlazor/Services/SatService.Windows.cs
@@ -1,24 +1,30 @@
-﻿namespace ElginM10MauiBlazor.Services;
+﻿using ElginM10MauiBlazor.Services.External.Windows;
+
+using System.Runtime.InteropServices;
+
+namespace ElginM10MauiBlazor.Services;
 internal partial class SatService
 {
     private partial void DoConstructor() { }
 
-    internal partial string AssociarAssinatura(int num_session, string activation_code, string cnpj_set, string sign_ac) => throw new NotImplementedException();
-    internal partial string AtivarSat(int num_session, int certification_type, string activation_code, string cnpj, int c_uf) => throw new NotImplementedException();
-    internal partial string AtualizarSoftwareSat(int num_session, string activation_code) => throw new NotImplementedException();
-    internal partial string BloquearSat(int num_session, string activation_code) => throw new NotImplementedException();
-    internal partial string CancelarUltimaVenda(int num_session, string activation_code, string key, string cancellation_data) => throw new NotImplementedException();
-    internal partial string ComunicarCertificadoIcpbrasil(int num_session, string activation_code, string transaction_data) => throw new NotImplementedException();
-    internal partial string ConfigurarInterfaceDeRede(int num_session, string activation_code, string network_config) => throw new NotImplementedException();
-    internal partial string ConsultarNumeroSessao(int num_session, string activation_code, int consult) => throw new NotImplementedException();
-    internal partial string ConsultarSat(int num_session) => throw new NotImplementedException();
-    internal partial string ConsultarStatusOperacional(int num_session, string activation_code) => throw new NotImplementedException();
+        // => Marshal.PtrToStringUTF8(Sat.ConsultarSAT(num_session));
+
+    internal partial string AssociarAssinatura(int num_session, string activation_code, string cnpj_set, string sign_ac) => Marshal.PtrToStringUTF8(Sat.AssociarAssinatura(num_session, activation_code, cnpj_set, sign_ac));
+    internal partial string AtivarSat(int num_session, int certification_type, string activation_code, string cnpj, int c_uf) => Marshal.PtrToStringUTF8(Sat.AtivarSAT(num_session, certification_type, activation_code, cnpj, c_uf));
+    internal partial string AtualizarSoftwareSat(int num_session, string activation_code) => Marshal.PtrToStringUTF8(Sat.AtualizarSoftwareSAT(num_session, activation_code));
+    internal partial string BloquearSat(int num_session, string activation_code) => Marshal.PtrToStringUTF8(Sat.BloquearSAT(num_session, activation_code));
+    internal partial string CancelarUltimaVenda(int num_session, string activation_code, string key, string cancellation_data) => Marshal.PtrToStringUTF8(Sat.CancelarUltimaVenda(num_session, activation_code, key, cancellation_data));
+    internal partial string ComunicarCertificadoIcpbrasil(int num_session, string activation_code, string transaction_data) => Marshal.PtrToStringUTF8(Sat.ComunicarCertificadoICPBRASIL(num_session, activation_code, transaction_data));
+    internal partial string ConfigurarInterfaceDeRede(int num_session, string activation_code, string network_config) => Marshal.PtrToStringUTF8(Sat.ConfigurarInterfaceDeRede(num_session, activation_code, network_config));
+    internal partial string ConsultarNumeroSessao(int num_session, string activation_code, int consult) => Marshal.PtrToStringUTF8(Sat.ConsultarNumeroSessao(num_session, activation_code, consult));
+    internal partial string ConsultarSat(int num_session) => Marshal.PtrToStringUTF8(Sat.ConsultarSAT(num_session));
+    internal partial string ConsultarStatusOperacional(int num_session, string activation_code) => Marshal.PtrToStringUTF8(Sat.ConsultarStatusOperacional(num_session, activation_code));
     internal partial string ConsultarUltimaSessaoFiscal(int num_session, string activation_code) => throw new NotImplementedException();
-    internal partial string DesbloquearSat(int num_session, string activation_code) => throw new NotImplementedException();
-    internal partial string EnviarDadosVenda(int num_session, string activation_code, string transaction_data) => throw new NotImplementedException();
-    internal partial string ExtrairLogs(int num_session, string activation_code) => throw new NotImplementedException();
+    internal partial string DesbloquearSat(int num_session, string activation_code) => Marshal.PtrToStringUTF8(Sat.DesbloquearSAT(num_session, activation_code));
+    internal partial string EnviarDadosVenda(int num_session, string activation_code, string transaction_data) => Marshal.PtrToStringUTF8(Sat.EnviarDadosVenda(num_session, activation_code, transaction_data));
+    internal partial string ExtrairLogs(int num_session, string activation_code) => Marshal.PtrToStringUTF8(Sat.ExtrairLogs(num_session, activation_code));
     internal partial int GerarNumeroSessao() => new Random().Next(1, 999_999);
-    internal partial string TesteFimAFim(int num_session, string activation_code, string transaction_data) => throw new NotImplementedException();
-    internal partial string TrocarCodigoDeAtivacao(int num_session, string current_activation_code, int option, string new_code, string new_code_confirmation) => throw new NotImplementedException();
+    internal partial string TesteFimAFim(int num_session, string activation_code, string transaction_data) => Marshal.PtrToStringUTF8(Sat.TesteFimAFim(num_session, activation_code, transaction_data));
+    internal partial string TrocarCodigoDeAtivacao(int num_session, string current_activation_code, int option, string new_code, string new_code_confirmation) => Marshal.PtrToStringUTF8(Sat.TrocarCodigoDeAtivacao(num_session, current_activation_code, option, new_code, new_code_confirmation));
 
 }
